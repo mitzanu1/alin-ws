8fb40cadd5ccfae33b82453c31523a4da1b3e008
Mihai Mataoanu <mitzanu@hotmail.com>
2024-09-04 16:25:06 +0300

    portfolio initial

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -1,13 +1,48 @@
 'use client'
 import styles from './portfolio.module.css'
+import { useTranslation } from 'react-i18next';
 
 export default function Portfolio () {
 
+  const { t } = useTranslation()
 
     return (
         <section className={styles.main} id='portfolio'>
             <h1>Portfolio</h1>
-            <p className={styles.mp}>Facebook Twiter Spotify Teams si multe altele</p>
+            <div className={styles.grid}>
+                <div className={styles.card}>
+                    <div className={styles.img}>
+                        <img src='' />
+                    </div>
+                    <div className={styles.desc}>
+                        <p>{t('portfolio1-desc')}</p>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.img}>
+                        <img src='' />
+                    </div>
+                    <div className={styles.desc}>
+                        <p>{t('portfolio1-desc')}</p>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.img}>
+                        <img src='' />
+                    </div>
+                    <div className={styles.desc}>
+                        <p>{t('portfolio1-desc')}</p>
+                    </div>
+                </div>
+                <div className={styles.card}>
+                    <div className={styles.img}>
+                        <img src='' />
+                    </div>
+                    <div className={styles.desc}>
+                        <p>{t('portfolio1-desc')}</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
