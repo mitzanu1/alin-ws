1cb12a8c4648b21ad7788235b52ff788d6732a38
Mihai Mataoanu <mitzanu@hotmail.com>
2024-09-22 19:09:06 +0300

    carousel

---
+++ app/components/Carousel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
+const Carousel = () => {
+  return (
+    <CarouselProvider
+     naturalSlideWidth={100}
+     naturalSlideHeight={120}
+     totalSlides={3}
+    >
+
+    <Slider>
+      <Slide index={0}>
+      <div className={styles.card}>
+                    <a href='https://www.avochat.net/' target='_blank'>
+                    <div className={styles.img}></div>
+                    <div className={styles.desc}>
+                        <h4>{t('pf1-name')}</h4>
+                        <p>{t('pf1-desc')}</p>
+                    </div>
+                    </a>
+                </div>
+      </Slide>
+      <Slide index={1}>Slide 2</Slide>
+      <Slide index={2}>Slide 3</Slide>
+    </Slider>
+
+    <ButtonBack>{"<"}</ButtonBack>
+    <ButtonNext>{">"}</ButtonNext>
+   </CarouselProvider>
+  );
+};
+export default Carousel;
--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -1,13 +1,18 @@
 'use client'
 import { useTranslation } from 'react-i18next';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import styles from './portfolio.module.css'
+
+import  Carousel  from './Carousel';
 
 export default function Portfolio () {
 
   const { t } = useTranslation()
 
     return (
+        <>
         <div className='wrap bg3' id='portfolio'>
         <section className={styles.main} >
             <h1>{t('pf-title')}</h1>
@@ -48,9 +53,63 @@ export default function Portfolio () {
                     </div>
                     </a>
                 </div>
-                
             </div>
         </section>
-        </div>
+    </div>
+        <CarouselProvider
+            naturalSlideWidth={300}
+            naturalSlideHeight={420}
+            totalSlides={3}
+        >
+            <Slider>
+                <Slide 
+                    index={0}
+                    className={styles.slide}
+                >
+                    <div className={styles.card}>
+                        <a href='https://www.avochat.net/' target='_blank'>
+                            <div className={styles.img}></div>
+                            <div className={styles.desc}>
+                                <h4>{t('pf1-name')}</h4>
+                                <p>{t('pf1-desc')}</p>
+                            </div>
+                        </a>
+                    </div>
+                </Slide>
+                <Slide 
+                    index={1}
+                    className={styles.slide}
+                >
+                    <div className={styles.card}>
+                        <a href='https://www.avochat.net/' target='_blank'>
+                            <div className={styles.img}></div>
+                            <div className={styles.desc}>
+                                <h4>{t('pf1-name')}</h4>
+                                <p>{t('pf1-desc')}</p>
+                            </div>
+                        </a>
+                    </div>
+                </Slide>
+                <Slide 
+                    index={2}
+                    className={styles.slide}
+                >
+                    <div className={styles.card}>
+                        <a href='https://www.avochat.net/' target='_blank'>
+                            <div className={styles.img}></div>
+                            <div className={styles.desc}>
+                                <h4>{t('pf1-name')}</h4>
+                                <p>{t('pf1-desc')}</p>
+                            </div>
+                        </a>
+                    </div>
+                </Slide>
+            </Slider>
+            <div className={styles.sldBtn}>
+                <ButtonBack className={styles.crsBtn}>{'<'}</ButtonBack>
+                <ButtonNext className={styles.crsBtn}>{'>'}</ButtonNext>
+            </div>
+        </CarouselProvider>
+    </>
     )
 }
