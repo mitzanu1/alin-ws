b60597cca86accba5b33dd1b6e7d6427011e4f4e
Mihai Mataoanu <mitzanu@hotmail.com>
2024-10-15 01:00:15 +0300

    cf-alerts

--- app/components/ContactForm.js
+++ app/components/ContactForm.js
@@ -7,75 +7,106 @@ import emailjs from '@emailjs/browser';
 import styles from './contactForm.module.css'
 
 export default function ContactForm() {
-    const {modal, setModal} = useContext(ModalContext)
-    const { t } = useTranslation()
+  const { modal, setModal } = useContext(ModalContext)
+  const [popup, setPopup] = React.useState('none')
+  const [popText, setPopText] = React.useState('')
+  const [bgc, setBgc] = React.useState('rgb(0, 216, 0)')
+  const [disabled, setDisabled] = React.useState(false)
+  const { t } = useTranslation()
 
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs
-        .sendForm('service_dcu2aqg', 'template_9xjif21', form.current, {
-          publicKey: 'l9SHxUEvWfYQLlppI',
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
-    };
-  
-    return (
-        <div className={styles.wrap}
-            style={{display:modal}}
-            onClick={()=>{setModal('none')}}
+  const sendEmail = async(e) => {
+    e.preventDefault();
+    setDisabled(true)
+    await emailjs
+      .sendForm('service_dcu2aqg', 'template_9xjif21', form.current, {
+        publicKey: 'l9SHxUEvWfYQLlppI',
+      })
+      .then(
+        () => {
+          setBgc('rgb(0, 216, 0)')
+          setPopText(t('alert-succes'))
+          setPopup('flex')
+        },
+        (error) => {
+          setBgc('rgb(255, 0, 76)')
+          setPopText(t('alert-error'))
+          setPopup('flex')
+          console.log('FAILED...', error.text);
+        },
+      );
+      setDisabled(false)
+  };
+
+  return (
+    <div className={styles.wrap}
+      style={{ display: modal }}
+      onClick={() => {
+        setPopup('none')
+        setModal('none')
+      }}
+    >
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className={styles.main}
+        onClick={(e) => { e.stopPropagation() }}
+      >
+        <h6
+          className={styles.close}
+          onClick={() => {
+            setPopup('none')
+            setModal('none')
+          }}
         >
-        <form 
-            ref={form}
-            onSubmit={sendEmail}
-            className={styles.main}
-            onClick={(e)=>{e.stopPropagation()}}
-        >
-            <h6 className={styles.close}
-                onClick={()=>setModal('none')}
-            >
-              ᙭</h6>
-            <p>{t('form-head')}</p>
-            <p>{t('form-head-email')}: <span>contact@bitgenesys.ro</span></p>
-            <div className={styles.detailsBox}>
-                <input 
-                  type="text"
-                  name="user_name"
-                  placeholder={t('form-name')}
-                />
-                <input
-                  type="email" 
-                  name="user_email" 
-                  placeholder={t('form-email')}
-                />
+          ᙭</h6>
+        <p>{t('form-head')}</p>
+        <p>{t('form-head-email')}: <span>contact@bitgenesys.com</span></p>
+        <div className={styles.detailsBox}>
+          <input
+            type="text"
+            name="user_name"
+            placeholder={t('form-name')}
+            required
+          />
+          <input
+            type="email"
+            name="user_email"
+            placeholder={t('form-email')}
+            required
+          />
 
-            </div>
-            <input 
-                type='text'
-                name='user_subject'
-                placeholder={t('form-subject')}
-            />
-            <textarea 
-                name='message'
-                placeholder={t('form-message')}
-                rows={7}
-            />
-            <button
-                className={styles.btn}
-                type='submit'
-                onClick={()=>setModal('none')}
-            >{t('send-message')}</button>
-            
-        </form>
         </div>
-    )
+        <input
+          type='text'
+          name='user_subject'
+          placeholder={t('form-subject')}
+          required
+        />
+        <textarea
+          name='message'
+          placeholder={t('form-message')}
+          rows={7}
+          required
+        />
+        <div
+          style={{ display: popup, backgroundColor: bgc }}
+          className={styles.popup}
+          onClick={() => {
+            setPopup('none')
+            setModal('none')
+          }}
+        >
+          <p>{popText}</p>
+        </div>
+        <button
+          className={styles.btn}
+          style={{ display: popup === 'flex' ? 'none' : 'flex' }}
+          type='submit'
+          disabled={disabled}
+        >{t('send-message')}</button>
+      </form>
+    </div>
+  )
 }
